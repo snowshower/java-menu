4d28e01fc24a7c0fe4c80fdabdbb953d07e402b7
snow___shower <wonsb25@gmail.com>
2026-01-07 16:56:39 +0900

    feat: 예외처리 로직 작성

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,17 +1,34 @@
 package menu.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import menu.util.Validator;
 
 public class InputView {
 
     public String inputCoachName(){
-        System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
-        return Console.readLine();
+        while(true){
+            try{
+                System.out.println("코치의 이름을 입력해 주세요. (, 로 구분)");
+                String input=Console.readLine();
+                Validator.validateCoachInput(input);
+                return input;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     public String inputBanMenu(String coachName){
         System.out.println();
-        System.out.println(coachName+"(이)가 못 먹는 메뉴를 입력해 주세요.");
-        return Console.readLine();
+        while(true){
+            try{
+                System.out.println(coachName+"(이)가 못 먹는 메뉴를 입력해 주세요.");
+                String input=Console.readLine();
+                Validator.validateMenuInput(input);
+                return input;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
