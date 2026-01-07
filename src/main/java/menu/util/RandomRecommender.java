139d90a0a9d096960b85dd9c697c470052f82fa9
snow___shower <wonsb25@gmail.com>
2026-01-07 15:41:55 +0900

    feat: 랜덤 추천기 작성

---
+++ src/main/java/menu/util/RandomRecommender.java
@@ -0,0 +1,18 @@
+package menu.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class RandomRecommender {
+    private static int START=1;
+    private static int LAST=5;
+
+    public static int recommendRandomCategory(){
+        return Randoms.pickNumberInRange(START, LAST);
+    }
+
+    public static String recommendRandomMenu(List<String> menus){
+        return Randoms.shuffle(menus).get(0);
+    }
+}
---
+++ src/test/java/menu/랜덤테스트.java
@@ -0,0 +1,24 @@
+package menu;
+
+import menu.domain.Category;
+import menu.util.RandomRecommender;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class 랜덤테스트 {
+
+    @Test
+    void 랜덤카테고리테스트(){
+        int i= RandomRecommender.recommendRandomCategory();
+        Assertions.assertThat(Category.from(i)).isBetween(Category.JAPANESE, Category.WESTERN);
+    }
+
+    @Test
+    void 랜덤메뉴테스트(){
+        List<String> menus=Category.JAPANESE.getMenus();
+        String s=RandomRecommender.recommendRandomMenu(menus);
+        Assertions.assertThat(menus.contains(s)).isTrue();
+    }
+}
