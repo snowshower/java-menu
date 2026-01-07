d45ef8e59a4d3830f85c48279143f3c43ab83586
snow___shower <wonsb25@gmail.com>
2026-01-07 16:18:42 +0900

    refactor: 메뉴 서비스 로직 수정

--- src/main/java/menu/service/MenuService.java
+++ src/main/java/menu/service/MenuService.java
@@ -5,15 +5,14 @@ import menu.domain.Coach;
 import menu.domain.Coaches;
 import menu.util.RandomRecommender;
 
+import java.util.Collections;
 import java.util.List;
 
 public class MenuService {
 
     public void recommendMenu(List<String> categories, Coaches coaches){
 
-        int categoryNum= RandomRecommender.recommendRandomCategory();
-        categories.add(Category.from(categoryNum).getCategoryName());
-
+        int categoryNum=recommendCategory(categories);
         for (Coach coach : coaches.getCoaches()) {
             while(true){
                 String menu=Category.getRandomMenu(categoryNum);
@@ -21,6 +20,18 @@ public class MenuService {
                 if(coach.isEatenMenu(menu)) continue;
                 coach.eat(menu); break;
             }
+        }
+    }
+
+    private int recommendCategory(List<String> categories){
+
+        while(true){
+            int categoryNum= RandomRecommender.recommendRandomCategory();
+            String categoryName=Category.from(categoryNum).getCategoryName();
+            int count= Collections.frequency(categories, categoryName);
+            if(count>2) continue;
+            categories.add(categoryName);
+            return categoryNum;
         }
     }
 }
